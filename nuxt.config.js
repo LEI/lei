4767d09ebf4c8eeeba32c1ee9ff327eabeae5246
Guillaume Frichet <guillaume.frichet@flexnetwork.fr>
2017-05-15 15:10:16 +0200

    ++

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
      ** Headers of the page
      */
     head: {
-        title: 'Guillaume',
+        title: 'LEI',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: 'Guillaume' },
+            { hid: 'description', name: 'description', content: '{#web, #dev, #design}' },
         ],
         link: [
             { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
