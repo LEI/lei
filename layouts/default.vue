e0ba44483ade0e32a532867895f6ddecaec65d2a
Guillaume Frichet <guillaume.frichet@gmail.com>
2017-05-21 04:58:20 +0200

    deploy

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,46 +1,19 @@
 <template>
     <!-- <le-header :title="title" :links="links"></le-header> -->
-    <!-- <header class="Header"> -->
-    <!--   <div class="container"> -->
-    <!--     <h1 class="Header__Title">Nuxt i18n</h1> -->
-    <!--     <nav class="Header__Menu"> -->
-    <!--       <nuxt-link class="Header__Link" :to="path('/')" exact> -->
-    <!--         {{ $t('links.home') }} -->
-    <!--       </nuxt-link> -->
-    <!--       <nuxt-link class="Header__Link" :to="path('/about')"  active-class="none"> -->
-    <!--         {{ $t('links.about') }} -->
-    <!--       </nuxt-link> -->
-    <!--       <nuxt-link class="Header__Link" v-if="$i18n.locale === 'en'" :to="`/fr` + $route.fullPath" active-class="none" exact> -->
-    <!--         {{ $t('links.french') }} -->
-    <!--       </nuxt-link> -->
-    <!--       <nuxt-link class="Header__Link" v-else :to="$route.fullPath.replace(/^\/[^\/]+/, '')" active-class="none" exact> -->
-    <!--         {{ $t('links.english') }} -->
-    <!--       </nuxt-link> -->
-    <!--     </nav> -->
-    <!--   </div> -->
-    <!-- </header> -->
-  <!-- <v-app top-toolbar left-fixed-sidebar class="elevation-1"> -->
-  <v-app top-toolbar class="elevation-1">
+  <v-app top-toolbar class="elevation-1"> <!-- left-fixed-sidebar -->
     <v-toolbar v-bind:class="theme.primary">
       <v-toolbar-side-icon class="grey--text text--darken-4" @click.native.stop="sidebar.visible = !sidebar.visible" />
       <!-- <v-toolbar-logo></v-toolbar-logo> -->
       <v-toolbar-title v-html="title"></v-toolbar-title>
       <v-toolbar-items>
-        <v-toolbar-item>Home</v-toolbar-item>
-        <v-toolbar-item>About</v-toolbar-item>
+        <v-toolbar-item v-for="(item,index) in nav" :key="index" href="item.href" :nuxt="item.nuxt">{{ item.text }}</v-toolbar-item>
         <v-menu bottom left offset-y origin="top right" transition="v-slide-y-transition" style="margin: 10px;">
           <v-btn icon dark slot="activator">
             {{ $i18n.locale }}
           </v-btn>
           <v-list>
-            <v-list-item v-for="(item, index) in locale_list" :key="index">
-              <v-list-tile :avatar="item.avatar || item.icon ? true : false" :href="item.href ? item.href : null" :nuxt="item.nuxt">
-                <!-- <v-list-tile-avatar v-if="item.icon"> -->
-                <!--   <v-icon>{{ item.icon }}</v-icon> -->
-                <!-- </v-list-tile-avatar> -->
-                <!-- <v-list-tile-avatar v-if="item.avatar"> -->
-                <!--   <img v-bind:src="item.avatar" /> -->
-                <!-- </v-list-tile-avatar> -->
+            <v-list-item v-for="(item, index) in lang_list" :key="index">
+              <v-list-tile :href="item.href ? item.href : null" :nuxt="item.nuxt">
                 <v-list-tile-title v-text="item.text"></v-list-tile-title>
               </v-list-tile>
             </v-list-item>
@@ -70,7 +43,7 @@
         <!--   </v-list-item> -->
         <!-- </v-list> -->
         <v-list two-line dense>
-          <template v-for="item in menu">
+          <template v-for="item in sidebar.menu">
             <v-subheader v-if="item.header" v-text="item.header" />
             <v-divider v-else-if="item.divider" v-bind:inset="item.inset" />
             <v-list-item v-else v-bind:key="item.text">
@@ -141,25 +114,29 @@ export default {
   //   }
   // },
   data () {
+    var nav = [
+        { icon: 'home', text: this.$t('links.home'), href: this.path('/'), nuxt: true },
+        { icon: 'account_box', text: this.$t('links.about'), href: this.path('/about'), nuxt: true },
+    ];
     return {
-      locale_list: [
-        { value: 'en', text: this.$t('links.english') },
-        { value: 'fr', text: this.$t('links.french') }
+      nav: nav,
+      lang_list: [
+        { value: 'en', text: this.$t('links.english'), href: this.$route.fullPath.replace(/^\/[^\/]+/, ''), nuxt: true },
+        { value: 'fr', text: this.$t('links.french'), href: `/fr` + this.$route.fullPath, nuxt: true }
       ],
       theme: { primary: 'green' },
       title: this.$t('home.title'),
-      sidebar: { visible: false },
-      menu: [
-          // { header: 'Pages' },
-          { icon: 'home', text: this.$t('links.home'), href: this.path('/'), nuxt: true },
-          { icon: 'account_box', text: this.$t('links.about'), href: this.path('/about'), nuxt: true },
-          { divider: true },
-          { header: 'Social' },
-          { text: 'Twitter', href: 'https://twitter.com/LEI', desc: 'twitter.com/LEI' },
-          { text: 'GitHub', href: 'https://github.com/LEI', desc: 'github.com/LEI' },
-          { text: 'Keybase', href: 'https://keybase.io/LEI', desc: 'keybase.io/LEI' },
-          // { url: '/', text: 'Back to home' },
-      ]
+      sidebar: {
+          visible: false,
+          menu: nav.concat([
+              { divider: true },
+              { header: 'Social' },
+              { text: 'Twitter', href: 'https://twitter.com/LEI', desc: 'twitter.com/LEI' },
+              { text: 'GitHub', href: 'https://github.com/LEI', desc: 'github.com/LEI' },
+              { text: 'Keybase', href: 'https://keybase.io/LEI', desc: 'keybase.io/LEI' },
+              // { url: '/', text: 'Back to home' },
+          ]),
+      },
     }
   },
   methods: {
